a8823c8ee143dda7c0cc116f0607a2f19f2fedba
Wasil <death888080@gmail.com>
2022-02-26 23:27:46 +0100

    exercise 1

---
+++ seminar_1/homework_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter num A: ");
+string a = Console.ReadLine();
+int num_1 = int.Parse(a);
+
+Console.WriteLine("Enter num B: ");
+string b = Console.ReadLine();
+int num_2 = int.Parse(b);
+
+if (num_1 > num_2)
+{
+    Console.WriteLine($"max number = {num_1}");
+}
+else
+{
+    Console.WriteLine($"max number = {num_2}");
+}
